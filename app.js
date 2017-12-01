b6d9a0ef97607e130fde9e5ac7b6a7723f68a2fd
manuasir <manu.asi2@gmail.com>
2017-12-01 21:34:30 +0100

    converting to standard style

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const BrewHTTP = require('./lib/brewHTTP');
-const BrewNode = require('./lib/brewNode');
+const BrewHTTP = require('./lib/brewHTTP')
+const BrewNode = require('./lib/brewNode')
 
-const port = 18070+Math.floor(Math.random()*30);
+const port = 18070 + Math.floor(Math.random() * 30)
 console.log('starting node on ', port)
 
-const http_port = 3000+Math.floor(Math.random()*10);
+const httpPort = 3000 + Math.floor(Math.random() * 10)
 
-let httpserver = new BrewHTTP(http_port,new BrewNode(port));
-
+let httpServer = new BrewHTTP(httpPort, new BrewNode(port))
+console.log('created httpServer at port ', httpServer.http_port)

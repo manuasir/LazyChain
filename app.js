bc84ce6cc29cd204a110ce600d342247a03c01a2
manuasir <manu.asi2@gmail.com>
2018-10-09 19:23:42 +0200

    Updating names

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const BrewHTTP = require('./lib/brewHTTP')
-const BrewNode = require('./lib/brewNode')
+const LazyHTTP = require('./lib/lazyHTTP')
+const LazyNode = require('./lib/lazyNode')
 
 const port = 18070 + Math.floor(Math.random() * 30)
 console.log('starting node on ', port)
 
 const httpPort = 3000 + Math.floor(Math.random() * 10)
 
-let httpServer = new BrewHTTP(httpPort, new BrewNode(port))
+let httpServer = new LazyHTTP(httpPort, new LazyNode(port))
 console.log('created httpServer at port ', httpServer.http_port)

4709e9898d3e987ed6d7b805dbfe8f9ddff292a8
CY Lim <cylim226@gmail.com>
2018-10-21 20:49:31 +0800

    add basic dockerfile

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const LazyHTTP = require('./lib/lazyHTTP')
 const LazyNode = require('./lib/lazyNode')
 
-const port = 18070 + Math.floor(Math.random() * 30)
+const port = process.env.NODE_PORT || 18070 + Math.floor(Math.random() * 30)
 console.log('starting node on ', port)
 
-const httpPort = 3000 + Math.floor(Math.random() * 10)
+const httpPort = process.env.HTTP_PORT || 3000 + Math.floor(Math.random() * 10)
 
 let httpServer = new LazyHTTP(httpPort, new LazyNode(port))
 console.log('created httpServer at port ', httpServer.http_port)
